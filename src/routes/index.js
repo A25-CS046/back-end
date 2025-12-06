2193404cd1b39526e65fecd8ecfd9f04fb4c3a9f
Vania <221110551@students.mikroskil.ac.id>
2025-12-06 12:05:35 +0700

    feat: integrate copilor routes into server

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./userRoutes");
+const copilotRoutes = require("./copilotRoutes");
 
 router.use("/users", userRoutes);
+router.use("/copilot", copilotRoutes);
 
 module.exports = router;
